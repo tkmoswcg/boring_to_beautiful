9b171cea39bed106d4caf656ba9d36a85229185d
Takumi Osawa <tkmoswcg@gmail.com>
2022-09-18 19:10:54 +0900

    Changed design layouts

--- lib/src/features/home/view/home_highlight.dart
+++ lib/src/features/home/view/home_highlight.dart
@@ -12,7 +12,7 @@ class HomeHighlight extends StatelessWidget {
       children: [
         Expanded(
           child: Padding(
-            padding: const EdgeInsets.all(2), // Modify this line
+            padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 5),
             child: Clickable(
               child: SizedBox(
                 height: 275,
